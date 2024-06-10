12bbb800dbbe2e026fe9f24af0b0e30d41942dfc
lightning <lightning@gltlab.net>
2024-06-10 12:24:04 +0000

    feat: basic functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
+import { Badge } from "@/components/ui/badge";
+import ChatBox from "@/components/chatbox";
+
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <div className='mx-auto flex min-h-screen w-full max-w-3xl flex-col space-y-6 p-4 md:p-8'>
+      <header className=''></header>
+      <main className='flex h-full w-full flex-1 flex-col'>
+        <div className='mx-auto h-full flex-1 px-4 py-4 border-border w-full rounded-lg border backdrop-blur-[2px] md:p-6'>
+          <div className='mx-auto flex w-full flex-col gap-8'>
+            <div className='col-span-full flex items-start justify-between gap-1'>
+              <div className='flex min-w-0 flex-col gap-1'>
+                <h1 className='font-cal text-3xl'>Chrome AI Test Page</h1>
+                <p className='text-muted-foreground'>
+                  Running large language models locally in the browser
+                </p>
+                <span>
+                  <Badge>Gemini Nano</Badge>
+                </span>
+              </div>
+            </div>
+
+            <ChatBox />
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
--- src/components/chatbox.tsx
+++ src/components/chatbox.tsx
@@ -1,5 +1,164 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { useRef } from "react";
+
+const checkAI = async () => {
+  if (window.ai) {
+    if ((await window.ai.canCreateTextSession()) === "readily") {
+      return true;
+    }
+  }
+  return false;
+};
+
 export default function ChatBox() {
-  return <div></div>;
+  const rawChatHistory = useRef<any[]>([]);
+  const [endMessage, setEndMessage] = useState<null | HTMLDivElement>(null);
+  const [model, setModel] = useState({
+    prompt: async (inputValue?: string) => {},
+  });
+  const [isAI, setIsAI] = useState<null | boolean>(null);
+  const [inputValue, setInputValue] = useState("");
+  const [chatHistory, setChatHistory] = useState(rawChatHistory.current);
+
+  const updateIsAI = async () => {
+    const checkAIStatus = await checkAI();
+
+    if (checkAIStatus) {
+      const thisModel = await window.ai.createTextSession();
+      setModel(thisModel);
+    }
+
+    setIsAI(checkAIStatus);
+  };
+
+  useEffect(() => {
+    updateIsAI();
+  }, []);
+
+  useEffect(() => {
+    endMessage?.scrollIntoView({ behavior: "smooth" });
+  }, [endMessage]);
+
+  return (
+    <>
+      <div>
+        {isAI === null && <p>Checking your browser</p>}
+        {isAI !== null &&
+          (isAI ? (
+            <p className='text-sm font-medium leading-none'>
+              Your chrome support Built-in AI. All code runs locally on your
+              computer. No internet.
+            </p>
+          ) : (
+            <p>
+              Built-in AI not work. Follow{" "}
+              <a
+                href='https://x.com/lightning_joyce/status/1800138565411799099'
+                className='font-medium text-primary underline underline-offset-4'
+              >
+                this steps
+              </a>{" "}
+              to turn on Built-in AI.
+            </p>
+          ))}
+      </div>
+
+      <div className='w-full'>
+        <div id='chatbox' className='p-4 h-[40vh] overflow-y-auto'>
+          {chatHistory.map((chat) => {
+            if (chat.role === "user") {
+              return (
+                <div className='mb-2 text-right' key={chat.id}>
+                  <p className='bg-blue-500 text-white rounded-lg py-2 px-4 inline-block'>
+                    {chat.text}
+                  </p>
+                </div>
+              );
+            } else {
+              return (
+                <div
+                  className='mb-2'
+                  key={chat.id}
+                  ref={(el) => {
+                    setEndMessage(el);
+                  }}
+                >
+                  <p className='bg-gray-200 text-gray-700 rounded-lg py-2 px-4 inline-block'>
+                    {chat.text}
+                  </p>
+                </div>
+              );
+            }
+          })}
+        </div>
+        <form
+          className='flex w-full items-center space-x-2'
+          onSubmit={async (form) => {
+            form.preventDefault();
+            if (inputValue === "") {
+              return;
+            }
+            rawChatHistory.current.push({
+              id: rawChatHistory.current.length + 1,
+              role: "user",
+              text: inputValue,
+            });
+            setChatHistory(rawChatHistory.current);
+
+            const prompt = `${rawChatHistory.current.map((chat) => {
+              return `${chat.role}: ${chat.text}\n`;
+            })}\nassistant:`;
+            const aiReplay = await model.prompt(prompt);
+
+            rawChatHistory.current.push({
+              id: rawChatHistory.current.length + 1,
+              role: "assistant",
+              text: aiReplay,
+            });
+            setChatHistory(rawChatHistory.current);
+            setInputValue("");
+          }}
+          onReset={() => {
+            rawChatHistory.current = [];
+            setChatHistory([]);
+            setInputValue("");
+          }}
+        >
+          <Button type='reset' disabled={!isAI}>
+            New Chat
+          </Button>
+          <Input
+            placeholder='Type here'
+            name='text'
+            value={inputValue}
+            onInput={(e) => {
+              setInputValue(e.target.value);
+            }}
+            disabled={!isAI}
+          />
+          <Button type='submit' disabled={!isAI}>
+            Send
+          </Button>
+        </form>
+
+        <div className='mt-10'>
+          <p className='text-center'>
+            Made by{" "}
+            <a
+              href='https://twitter.com/lightning_joyce'
+              target='_blank'
+              className='font-medium text-primary underline underline-offset-4'
+            >
+              Lightning Joyce
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
